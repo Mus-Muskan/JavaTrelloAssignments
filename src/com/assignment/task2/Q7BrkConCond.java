d0981d24e782fcfa2df9e44b61bfc6320d82fe1b
Mus-Muskan <94831102+Mus-Muskan@users.noreply.github.com>
2023-02-02 14:02:50 -0500

    Update Q7BrkConCond.java
    Code updated

--- src/com/assignment/task2/Q7BrkConCond.java
+++ src/com/assignment/task2/Q7BrkConCond.java
@@ -1,4 +1,30 @@
 package com.assignment.task2;
 
-public class Q7BrkConCond {
+public class Q7BrkConCond 
+{
+        public static void main(String[] args) 
+        {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the Integer value: ");
+            int value = sc.nextInt();
+            while (value!=0)
+            {
+                if (value>0)
+                {
+                  System.out.println("Good Going");
+                } 
+                else if (value<0) {
+                  System.out.println("It's Over");
+                  break;
+                }
+                System.out.print("Enter the Integer value: ");
+                int value1 = sc.nextInt();
+                value=value1;
+                continue;
+            }
+            if (value==0)
+            {
+              System.out.println("It is neither positive nor negative number");
+            }
+        }
 }
